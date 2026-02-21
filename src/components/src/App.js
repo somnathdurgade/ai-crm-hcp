9831dfa6bb4e6274fd9e0482097cc02103fc30ea
somnathdurgade <somnath.s.durgade@gmail.com>
2026-02-21 15:09:59 +0530

    Create App.js

---
+++ src/components/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LogInteractionScreen from "./components/LogInteractionScreen";
+
+function App() {
+  return (
+    <div>
+      <LogInteractionScreen />
+    </div>
+  );
+}
+
+export default App;
